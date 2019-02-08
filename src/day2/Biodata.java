5f91411dafd68a005bc29bcb689e3867ea6f128a
NSUIT-LAB <sabrinameem1@gmail.com>
2019-02-08 16:39:43 +0600

    biodata

---
+++ src/day2/Biodata.java
@@ -0,0 +1,12 @@
+package day2;
+
+import jdk.nashorn.internal.codegen.MethodEmitter;
+
+public class Biodata {
+    public static void main (String[]args){
+        System.out.println("Name:Sabrina Mannan Meem");
+        System.out.println("ID:1821319042");
+        System.out.println("Dept:ECE");
+
+    }
+}
